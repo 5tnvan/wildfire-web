166b6f490b33e44e1269bce512d9fad8a4256fdd
vtasst <5tnvan@gmail.com>
2024-07-02 16:33:01 +0200

    3seconds me callback

--- packages/nextjs/utils/login.tsx
+++ packages/nextjs/utils/login.tsx
@@ -28,7 +28,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `http://localhost:3000/callback`,
+      redirectTo: `http://3seconds.me/callback`,
     },
   });
 
