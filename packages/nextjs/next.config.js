8b27baffeb3731a81be12c7baec47bba8d5eb01b
vtasst <5tnvan@gmail.com>
2024-12-18 19:33:08 +0100

    - test: remove protected pages, exclude home

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -25,15 +25,15 @@ const nextConfig = {
       "aceternity.com",
     ],
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/home",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
