af301290d92e895739b34de389ad3ce596d23b8b
vtasst <5tnvan@gmail.com>
2024-12-19 09:47:49 +0100

    - lint

--- packages/nextjs/app/(protected)/sparks/page.tsx
+++ packages/nextjs/app/(protected)/sparks/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
